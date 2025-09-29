7ebc2221b11a19a0160752ef0ed749c36c41f859
sang <sang@DESKTOP-VPOH70L>
2025-09-30 01:48:44 +0700

    nop lab 3

--- Lab3/src/main/java/com/mycompany/lab3/bai2.java
+++ Lab3/src/main/java/com/mycompany/lab3/bai2.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.lab3;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ICT
@@ -12,5 +14,13 @@ public class bai2 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.printf("%d x %d = %d",i,j,i*j);
+                System.out.println(" ");
+            }
+            
+        }
     }
 }
--- Lab3/src/main/java/com/mycompany/lab3/bai3.java
+++ Lab3/src/main/java/com/mycompany/lab3/bai3.java
@@ -4,6 +4,10 @@
 
 package com.mycompany.lab3;
 
+import java.lang.reflect.Array;
+import java.util.Scanner;
+import java.util.Arrays;
+
 /**
  *
  * @author ICT
@@ -11,6 +15,38 @@ package com.mycompany.lab3;
 public class bai3 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        int mang[] ;
+        int sl;
+        System.out.println("nhap sl phan tu mang");
+        sl = sc.nextInt();
+        mang = new int[sl];
+        System.out.println("nhap phan tu mang");
+        for (int i = 0; i < sl; i++) {
+            mang[i] = sc.nextInt();
+        }
+        System.out.printf("mang cua ban la : " );
+        for (int i = 0; i < sl; i++) {
+            System.out.printf(mang[i] + " ");
+            
+            
+        }
+        Arrays.sort(mang);
+        System.out.printf("mang sau khi da sap xep");
+        for (int i = 0; i < sl; i++) {
+            System.out.printf(mang[i]+" ");
+        }
+        System.out.println("so nho nhat trong mang la :"+mang[0]);
+        int tong = 0;
+        int dem = 0;
+        for (int i = 0; i < sl; i++) {
+            if(mang[i]%3==0){
+                tong = tong + mang[i];
+                dem++;
+            }
+        }
+        int TBC = tong/dem;
+        System.out.printf("TBC cua mang la : "+TBC);
+        
     }
 }
